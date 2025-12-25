60795bb72632e530b4c9605aa382ff5e6af78040
bhaskarbidyanta <bhaskar.bid18@gmail.com>
2025-12-26 01:31:36 +0530

    frontend dev starts

---
+++ train-frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import ReactDOM,{ createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import "./styles/app.css"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
